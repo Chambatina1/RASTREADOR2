29dfe2e384ebac275afb1c5c68da6861109f9dda
Chambatina1 <geochambatina@gmail.com>
2026-04-16 22:59:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -345,6 +345,4 @@ app.post("/api/chat", async (req, res) => {
   }
 });
 
-// ================= INICIO =================
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Servidor Chambatina corriendo en puerto ${PORT}`));
+
